a23b850057e8fec5ca6156807390ea0b80d2cb85
tochien23 <tovanchien@dtu.edu.vn>
2024-09-27 23:18:29 +0700

    Build Base Basic Project

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+import clientRoutes from "./routes/client/index.route";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+//Client Routes
+clientRoutes(app);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
---
+++ routes/client/topic.route.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+const router: Router = Router();
+
+import * as controller from "../../controllers/client/topic.controller";
+
+router.get("/", controller.topics);
+
+
+export const topicRoutes: Router = router;
